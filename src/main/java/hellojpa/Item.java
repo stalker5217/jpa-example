68d074cdbfba1e7b34d5a1cfe54d792a95b126d8
Song <stalker5217@gmail.com>
2021-09-26 17:29:24 +0900

    cache clear

---
+++ src/main/java/hellojpa/Item.java
@@ -0,0 +1,23 @@
+package hellojpa;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class Item {
+    @Id @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+}
